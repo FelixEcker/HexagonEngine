cf681ac3242767de7c8facb86e745a5be2763943
Bertrahm <31585250+FelixEcker@users.noreply.github.com>
2020-10-12 22:06:37 +0200

    Added a JSONResourceLoader
    Used for loading Resources listed in a json file.
    
    (Example found in the test-package resources as "resources.json")

---
+++ src/main/java/de/hexagonsoftware/engine/resources/JSONResourceLoader.java
@@ -0,0 +1,41 @@
+package de.hexagonsoftware.engine.resources;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+
+import de.hexagonsoftware.engine.HexagonEngine;
+
+public class JSONResourceLoader {
+	private static Logger logger = LogManager.getLogger("JSONResourceLoader");
+	
+	public static void loadResources(String file) throws FileNotFoundException {
+		Gson gson = new Gson();
+		JsonReader reader = new JsonReader(new InputStreamReader(HexagonEngine.getGame().getClass().getResourceAsStream(file)));
+		JsonObject resourceFile = gson.fromJson(reader, JsonObject.class);
+		
+		if (resourceFile.has("textures")) {
+			loadTextures(resourceFile);
+		}
+	}
+	
+	public static void loadTextures(JsonObject resourceFile) {
+		JsonObject textures = resourceFile.get("textures").getAsJsonObject();
+		
+		int loadedTextures = 0;
+		
+		for (String texture : textures.keySet()) {
+			loadedTextures++;
+			HexagonEngine.HE_RES_MANAGER.addResource(texture, new TextureResource(textures.get(texture).getAsString()));
+		}
+		
+		logger.info("Loaded "+loadedTextures+" textures");
+	}
+}
--- src/test/java/de/hexagonsoftware/test/layeredrendering/LayeredRenderingTest.java
+++ src/test/java/de/hexagonsoftware/test/layeredrendering/LayeredRenderingTest.java
@@ -1,14 +1,22 @@
 package de.hexagonsoftware.test.layeredrendering;
 
 import java.awt.Graphics;
+import java.io.FileNotFoundException;
 
 import de.hexagonsoftware.engine.HexagonEngine;
 import de.hexagonsoftware.engine.game.IGame;
+import de.hexagonsoftware.engine.resources.JSONResourceLoader;
 
 public class LayeredRenderingTest implements IGame {
 
 	@Override
 	public void start() {
+		try {
+			JSONResourceLoader.loadResources("/resources.json");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		HexagonEngine.HE_GOBJ_MANAGER.addGameObject("test", new TestObject());
 	}
 
