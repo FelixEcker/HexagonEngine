cc14e765cb55be666146508dc2ea78477d168da3
Bertrahm <31585250+FelixEcker@users.noreply.github.com>
2020-10-15 13:06:39 +0200

    Updated LayeredRendering demo to new changes

--- src/test/java/de/hexagonsoftware/test/layeredrendering/LayeredRenderingTest.java
+++ src/test/java/de/hexagonsoftware/test/layeredrendering/LayeredRenderingTest.java
@@ -1,21 +1,24 @@
 package de.hexagonsoftware.test.layeredrendering;
 
 import java.awt.Graphics;
-import java.io.FileNotFoundException;
 
 import de.hexagonsoftware.engine.HexagonEngine;
 import de.hexagonsoftware.engine.game.IGame;
 import de.hexagonsoftware.engine.resources.JSONResourceLoader;
+import de.hexagonsoftware.engine.util.HEResourceLoadException;
 
 public class LayeredRenderingTest implements IGame {
-
 	@Override
-	public void start() {
+	public void init() {
 		try {
 			JSONResourceLoader.loadResources("/resources.json");
-		} catch (FileNotFoundException e) {
+		} catch (HEResourceLoadException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	@Override
+	public void start() {
 		HexagonEngine.HE_GOBJ_MANAGER.addGameObject("test", new TestObject());
 		System.out.println(HexagonEngine.HE_RES_MANAGER.getTextureResource("CharacterFRONT").getImage());
 	}
--- src/test/java/de/hexagonsoftware/test/layeredrendering/TestObject.java
+++ src/test/java/de/hexagonsoftware/test/layeredrendering/TestObject.java
@@ -3,6 +3,7 @@ package de.hexagonsoftware.test.layeredrendering;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import de.hexagonsoftware.engine.HexagonEngine;
 import de.hexagonsoftware.engine.game.GameObject;
 import de.hexagonsoftware.engine.game.components.LayeredRenderer;
 import de.hexagonsoftware.engine.game.components.Renderer;
@@ -23,8 +24,11 @@ public class TestObject extends GameObject {
 			@Override
 			public void render(Graphics g) {
 				Color oldColor = g.getColor();
+				g.setColor(Color.BLACK);
+				g.fillRect(0, 0, HexagonEngine.getWidth(), HexagonEngine.getHeight());
 				g.setColor(Color.BLUE);
 				g.fillOval(100, 100, 50, 50);
+				g.setColor(oldColor);
 			}
 		};
 		
@@ -35,12 +39,24 @@ public class TestObject extends GameObject {
 				Color oldColor = g.getColor();
 				g.setColor(Color.RED);
 				g.fillOval(111, 111, 25, 25);
+				g.setColor(oldColor);
+			}
+		};
+		
+		Renderer layer3 = new Renderer() {
+			@Override
+			public void render(Graphics g) {
+				Color oldColor = g.getColor();
+				g.setColor(Color.YELLOW);
+				g.drawString("FPS: "+HexagonEngine.getHE_FPS(), 10, 30);
+				g.setColor(oldColor);
 			}
 		};
 		
 		// Add both layers to the LayeredRenderer
 		lr.addLayer(layer1, true);
 		lr.addLayer(layer2, true);
+		lr.addLayer(layer3, true);
 		
 		// Finally add the LayeredRenderer to this objects Components
 		addComponent("layeredRenderer", lr);
