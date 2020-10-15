1aa449790c4b072dd8a406965500f28199e85edb
Bertrahm <felix_eckert_@t-online.de>
2020-10-15 23:44:09 +0200

    Added getter for last MouseEvent

--- src/main/java/de/hexagonsoftware/engine/input/Mouse.java
+++ src/main/java/de/hexagonsoftware/engine/input/Mouse.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseEvent;
 
 public class Mouse extends MouseAdapter {
 	public int buttonClicked;
+	public MouseEvent lastMouseEvent;
 	
 	public Mouse() {
 		buttonClicked = -1;
@@ -13,14 +14,20 @@ public class Mouse extends MouseAdapter {
 	@Override
 	public void mousePressed(MouseEvent e) {
 		buttonClicked = e.getButton();
+		lastMouseEvent = e;
 	}
 	
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		buttonClicked = -1;
+		lastMouseEvent = e;
 	}
 	
 	public boolean wasButtonClicked(int button) {
 		return button == buttonClicked;
+	}
+	
+	public MouseEvent getLastMouseEvent() {
+		return lastMouseEvent;
 	}
 }
