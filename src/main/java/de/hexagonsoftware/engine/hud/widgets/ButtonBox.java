5e135a9545ce7fbd5f240e9dace73eb494236c84
Bertrahm <felix_eckert_@t-online.de>
2020-10-26 15:50:44 +0100

    Added A ButtonBox for multiple buttons

---
+++ src/main/java/de/hexagonsoftware/engine/hud/widgets/ButtonBox.java
@@ -0,0 +1,129 @@
+package de.hexagonsoftware.engine.hud.widgets;
+
+import java.awt.Graphics;
+import java.awt.Point;
+
+import de.hexagonsoftware.engine.HexagonEngine;
+
+public class ButtonBox extends HUDWidget {	
+	public static int 
+	TOP_LEFT      = 0, 
+	TOP_RIGHT     = 1, 
+	BOTTOM_LEFT   = 2,
+	BOTTOM_RIGHT  = 3, 
+	TOP_CENTER 	  = 4, 
+	BOTTOM_CENTER = 5, 
+	CENTER 		  = 6
+	;
+	
+	private int alignment;
+	private ImageButton[] buttons;
+	private int[][] positions;
+	
+	private int ticks = 0;
+	
+	public ButtonBox(int x, int y) {
+		super(x, y);
+	}
+	
+	public ButtonBox(int x, int y, int alignment) {
+		super(x, y);
+		this.alignment = alignment;
+	}
+	
+	public ButtonBox(int x, int y, int alignment, ImageButton[] buttons) {
+		super(x, y);
+		this.alignment = alignment;
+		this.buttons = buttons;
+	}
+
+	public Point getButtonPosition(ImageButton button) {
+		Point location = null;
+		int buttonWidth = button.getWidth();
+		int buttonHeight = button.getHeight();
+		
+		switch (alignment) {
+		case 0:
+			location = new Point(0, buttonHeight);
+			break;
+		case 1:
+			location = new Point(HexagonEngine.getWidth()-buttonWidth, buttonHeight);
+			break;
+		case 2:
+			location = new Point(0, HexagonEngine.getHeight()-buttonHeight);
+			break;
+		case 3:
+			location = new Point(HexagonEngine.getWidth()-buttonWidth, HexagonEngine.getHeight()-buttonHeight);
+			break;
+		case 4:
+			location = new Point((int) ((HexagonEngine.getWidth()/2)-buttonWidth/2), buttonHeight);
+			break;
+		case 5:
+			location = new Point((int) ((HexagonEngine.getWidth()/2)-buttonWidth/2), HexagonEngine.getHeight()-buttonHeight);
+			break;
+		case 6:
+			location = new Point((int) ((HexagonEngine.getWidth()/2)-buttonWidth/2), (HexagonEngine.getHeight()/2)-(buttonHeight/2));
+			break;
+		}
+		
+		return location;
+	}
+	
+	public void calculatePositions() {
+		if (buttons.length < 0)
+			return;
+		
+		positions = new int[buttons.length][buttons.length*2];
+
+		this.height = 0;
+		for (ImageButton button : buttons) {
+			this.height = height + button.height;
+		}
+		
+		int buttonHeightAverage = this.height/buttons.length;
+		int prevButtonHeight = 0;
+		for (int i = 0; i < buttons.length; i++) {
+			prevButtonHeight = i != 0 ? buttons[i-1].getHeight() + prevButtonHeight : 0;
+			Point pos = getButtonPosition(buttons[i]);
+			pos.setLocation(pos.getX(), pos.getY()-prevButtonHeight);
+			buttons[i].setX((int) pos.getX());
+			buttons[i].setY((int) pos.getY()+buttonHeightAverage);
+		}
+	}
+
+	@Override
+	public void update() {
+		ticks++;
+		
+		for (ImageButton button : buttons) {
+			button.update();
+		}
+		
+		if (ticks % 60 == 0) {
+			calculatePositions();
+		}
+	}
+	
+	@Override
+	public void render(Graphics g) {
+		for (ImageButton button : buttons) {
+			button.render(g);
+		}
+	}
+	
+	public int getAlignment() {
+		return alignment;
+	}
+
+	public ImageButton[] getButtons() {
+		return buttons;
+	}
+
+	public void setAlignment(int alignment) {
+		this.alignment = alignment;
+	}
+
+	public void setButtons(ImageButton[] buttons) {
+		this.buttons = buttons;
+	}
+}
